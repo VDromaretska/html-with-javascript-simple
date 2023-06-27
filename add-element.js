99145ae111ebe52598eb9e204470c1075f17f4c4
VDromaretska <viktoriya.dromaretska@gmail.com>
2023-06-27 18:30:14 +0200

    created new heading in js

--- add-element.js
+++ add-element.js
@@ -1,12 +1,15 @@
 // create a new div element
-const newDiv = document.createElement("div");
+//const newDiv = document.createElement("div");
+const newHeading = document.createElement("h2");
 
 // and give it some content
-const newContent = document.createTextNode("Hi there and greetings!");
+//const newContent = document.createTextNode("Hi there and greetings!");
+const newContent = document.createTextNode("Coding is fun");
 
 // add the text node to the newly created div
-newDiv.appendChild(newContent);
+//newDiv.appendChild(newContent);
+newHeading.appendChild(newContent);
 
 // add the newly created element and its content into the DOM
 const existingDiv = document.getElementById("my-target-element");
-document.body.appendChild(newDiv, existingDiv);
+document.body.appendChild(newHeading, existingDiv);
